a79f0aab1a759c2954468d745ccd68d52c50c4f1
bartekl <bartekl@2a.pl>
2017-03-10 20:18:00 +0100

    canvas fix 4

--- base/scripts/lib/install_canvas.sh
+++ base/scripts/lib/install_canvas.sh
@@ -3,7 +3,7 @@
 set -e
 
 apt-get -y install libcairo2-dev 
-apt-get -y install libjpeg8-dev 
+apt-get -y install libjpeg-dev 
 apt-get -y install libpango1.0-dev 
 apt-get -y install libgif-dev 
 apt-get -y install build-essential g++
